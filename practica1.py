1921ec02be6c5798a2ff3b65f424220f74ff518c
Juan Urena Garcia Gasco <jurenag@alpha28.aulas.gsyc.urjc.es>
2018-03-08 17:14:01 +0100

    falta corregir errores y optimizar el codigo

--- practica1.py
+++ practica1.py
@@ -12,13 +12,13 @@ j.urenag@alumnos.urjc.es
 import webapp
 import socket
 from urllib.parse import unquote
+import os.path
 
 url_short={}
 url_long={}
 
-#To Do deberia mostrar toda la biblioteca, no volver a si mismo
 
-#form action siempre devulve a barra, y en el caso de que me pasen una url buena ya pondre yo que tengo que mandar a otra pagina
+
 FORMULARIO= """
 <form action="" method="POST"> 
 URL que quiere guardar:<br>
@@ -28,27 +28,49 @@ URL que quiere guardar:<br>
 """
 
 
-data=open("data_url")
-
+if (os.path.isfile("data_url")):
+    data=open("data_url")
+else:
+    data=open("data_url", "w")
+    data.close()
+    data=open("data_url")
+   
 urls_code=data.read().split()
-print (urls_code)
+data.close()
+#print (urls_code)
+    
+#moverlo al if name main 
+for url in urls_code:
+    key=url.split(":",1)[0]
+    value=url.split(":",1)[1]
+    url_short[key] =value
+    url_long[value] =key
 
 class practica1(webapp.webApp):
 
     def parse(self, request):
         """Parse the received request, extracting the relevant information."""
-        
-        method=request.split()[0]
-        resource=request.split()[1]
-
+        if (request):
+            method=request.split()[0]
+            resource=request.split()[1]
+        else:
+            method=None
+            resource=None
+            request=None
+   
         return method, resource, request
-        #Cambiar nombre variable 
+        
+        
+#si me pasan url en blanco, comprobar.    
+#Cambiar nombre variable 
     def process(self, parsedRequest):
         """Process the relevant elements of the request.
         Returns the HTTP code for the reply, and an HTML page.
         """
         method, resource, request=parsedRequest
         text=""
+        answer=""
+        code="200 OK"
         if method=="GET":
             if resource=="/":
                 code="200 OK"
@@ -68,51 +90,32 @@ class practica1(webapp.webApp):
             body=request.split('\r\n\r\n',1)[1]
             url=body.split('=')[1]
             print(url)
-            url=unquote(unquote(url))
-            print(url)
-            if not (url.startswith('http://') or url.startswith('https://')):
-                url="http://"+url
-            if not (url in url_long):
-                pos="/"+str(len(url_short))
-                url_short[pos] =url
-                url_long[url] =pos
-                code="200 OK"
-                answer=("<html><body><h1>La URL se ha guardado correctamente</h1><a href="+url+">URL Original</a> </br><a href=http://localhost:1234"+pos+">URL corta</a></body></html>")
+            
+            if (url):
+                url=unquote(unquote(url))
+                print(url)
+                if not (url.startswith('http://') or url.startswith('https://')):
+                    url="http://"+url
+                if not (url in url_long):
+                    pos="/"+str(len(url_short))
+                    url_short[pos] =url
+                    url_long[url] =pos
+                    code="200 OK"
+                    answer=("<html><body><h1>La URL se ha guardado correctamente</h1><a href="+url+">URL Original</a> </br><a href=http://localhost:1234"+pos+">URL corta</a></body></html>")
+                    data=open("data_url", "a")
+                    data.write(pos+":"+url+"\n")
+                    data.close()
+                else: 
+                    print("La url ya estaba guardada") 
+                    code="200 OK"
+                    answer=("<html><body><h1>La URL ya estaba en la lista, su direccion es:</h1><a href="+url_long[url]+">http://localhost:1234"+url_long[url]+"</a> </body></html>")
             else: 
-#hay que hacer esta parte de mostrar donde esta url corta
-                print("Hay que mandar el codigo y demas") 
                 code="200 OK"
-                answer=("<html><body><h1>La URL ya estaba en la lista</h1></body></html>")
+                answer=("<html><h1>Rosu hermano quieres casarte conmigo <3 </h1></html>")
             
 
         return (code, answer)
-#Revisar si quitar.
-    def __init__(self, hostname, port):
-        """Initialize the web application."""
 
-        # Create a TCP objet socket and bind it to a port
-        mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        mySocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        mySocket.bind((hostname, port))
-
-        # Queue a maximum of 5 TCP connection requests
-        mySocket.listen(5)
-
-        # Accept connections, read incoming data, and call
-        # parse and process methods (in a loop)
-
-        while True:
-            print('Waiting for connections')
-            (recvSocket, address) = mySocket.accept()
-            print('HTTP request received (going to parse and process):')
-            request = recvSocket.recv(2048).decode('utf-8')
-            print(request)
-            parsedRequest = self.parse(request)
-            (returnCode, htmlAnswer) = self.process(parsedRequest)
-            print('Answering back...')
-            recvSocket.send(bytes("HTTP/1.1 " + returnCode + " \r\n\r\n"
-                            + htmlAnswer + "\r\n", 'utf-8'))
-            recvSocket.close()
             
 if __name__ == "__main__":
     testWebApp = practica1("localhost", 1234)
